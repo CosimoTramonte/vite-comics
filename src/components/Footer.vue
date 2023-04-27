aed43373627f01dec5f781b65cb91d37bde7f32f
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-27 17:29:10 +0200

    add footer top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,129 @@ export default {
 </script>
 
 <template>
-  <h1>Footer</h1>
+  <div class="footerTop-wrapper">
+    <div class="footerTop-container">
+      <div class="menus">
+        <div class="col">
+          <h4>DC COMICS</h4>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+
+          <h4>SHOP</h4>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4>DC</h4>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4>DC COMICS</h4>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+          </ul>
+        </div>
+      </div>
+      <img src="../assets/img/dc-logo-bg.png" alt="dc logo">
+    </div>
+  </div>
+
+  <div class="footerBottom-wrapper">
+    <div class="footerBottom-container">
+    
+    </div>
+  </div>
+
+
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+  @use "../scss/partials/utilities" as *;
+
+  .footerTop-wrapper{
+    background-image: url("../assets/img/footer-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    .footerTop-container{
+      width: 80%;
+      margin: 0 auto;
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      min-height: 500px;
+      overflow-y: hidden ;
+      display: flex;
+        align-items: center;
+
+      .menus{
+        display: flex;
+        align-items: flex-start;
+
+        .col{
+          margin-right:35px ;
+
+          h4{
+            color: white;
+            font-size: 20px;
+            margin-bottom: 20px;
+          }
+
+          ul{
+            list-style: none;
+            margin-bottom: 20px;
+
+            li{
+              margin: 5px 0;
+
+              a{
+                color: #959595;
+                text-decoration: none;
+              }
+            }
+
+          }
+        }
+
+      }
+
+      img{
+        position: absolute;
+        top: -75px;
+        zoom: 120%;
+        right: 0;
+      }
+    }
+  }
+  .footerBottom-wrapper{
+    background-color: #303030;
+    min-height: 150px;
+  }
 
 </style>
