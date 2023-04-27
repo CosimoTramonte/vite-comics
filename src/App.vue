caf7add57d0508cdda7a785adebd0264a98c90b7
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-27 14:19:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import SectionMain from "./components/SectionMain.vue";
+import Footer from "./components/Footer.vue";
+
+
+export default {
+  name:"App",
+  components:{
+    Header,
+    Main,
+    SectionMain,
+    Footer
+  },
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <Header/>
+  <Main/>
+  <SectionMain/>
+  <Footer/>
+</template>
+
+<style lang="scss" scoped>
+
+  @use "./scss/main.scss";
+
+
+</style>
---
+++ src/components/SectionMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "SectionMain"
+}
+</script>
+
+<template>
+  <h1>SectionMain</h1>
+</template>
+
+<style>
+
+</style>
